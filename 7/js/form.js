785752874cb9ecd5e413378fac57afe63562c8ad
Keks <keks@htmlacademy.ru>
2022-11-20 03:26:17 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/form.js
@@ -0,0 +1,37 @@
+import {isEscapeKey} from './util.js';
+
+const imageFormEditing = document.querySelector('.img-upload__overlay');
+const imageFormOpen = document.querySelector('.img-upload__input');
+const imageFormClose = imageFormEditing.querySelector('.img-upload__cancel');
+const modalOpen = document.querySelector('body');
+const imageForm = document.querySelector('.img-upload__form');
+
+const onFormEscKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeFormEditing();
+  }
+};
+
+function openFormEditing () {
+  imageFormEditing.classList.remove('hidden');
+  modalOpen.classList.add('modal-open');
+
+  document.addEventListener('keydown', onFormEscKeydown);
+}
+
+function closeFormEditing () {
+  imageFormEditing.classList.add('hidden');
+  modalOpen.classList.remove('modal-open');
+  imageForm.reset();
+  document.removeEventListener('keydown', onFormEscKeydown);
+}
+
+imageFormOpen.addEventListener('change', () => {
+  openFormEditing();
+});
+
+
+imageFormClose.addEventListener('click', () => {
+  closeFormEditing();
+});
---
+++ 7/js/main.js
@@ -0,0 +1,15 @@
+import './picture.js';
+import './picture-editor.js';
+import { createPhotoArray } from './data.js';
+import { createPhotoElement } from './picture.js';
+import './form.js';
+
+const picturesContainer = document.querySelector('.pictures');
+const similarPictures = createPhotoArray();
+const similarListFragment = document.createDocumentFragment();
+
+similarPictures.forEach((dataPic) => {
+  similarListFragment.append(createPhotoElement(dataPic));
+});
+
+picturesContainer.append(similarListFragment);
